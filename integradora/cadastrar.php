8d059c3d8dddc6f4319205ca59b4374a1c132164
Newmann Naba Costa <99230382+Newmann2018@users.noreply.github.com>
2024-02-29 22:53:57 -0300

    Add files via upload

---
+++ integradora/cadastrar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <div id="corpo-form-cad">
+    <h1>CADASTRAR</h1>
+    <form method="Post" action="processa.php">
+    <input type="text" name="" placeholder="digite seu Nome Completo">
+    <input type="nunber" name="" placeholder="digite seu telefone">
+        <input type="email" name="" placeholder="digite seu email">
+        <input type="password" name="" placeholder="digite sua senha">
+        <input type="password" name="" placeholder="confirme sua senha">
+        <input type="submit" name="" value="CADASTRAR">
+    </form>
+    </div>
+</body>
+</html>
---
+++ integradora/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <div id="corpo-form">
+    <h1>Entrar</h1>
+    <form method="Post" action="processa.php">
+        <input type="email" name="" placeholder="digite seu email">
+        <input type="password" name="" placeholder="digite sua senha">
+        <input type="submit" name="" value="ACESSAR">
+        <a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se!</strong></a>
+    </form>
+    </div>
+</body>
+</html>
